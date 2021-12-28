9d07f48517b78c2f84e5b13e629fdce2758661c3
Nycole Xavier <nycole.xavierr@gmail.com>
2021-12-28 10:01:37 -0400

    add

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { RepositoryList } from './componentes/RepositoryList';
+
+import './styles/global.scss';
+
+export function App(){
+    return (
+        <>
+        <RepositoryList/>
+        </>
+    )
+}
